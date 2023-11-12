b367eb41f179b2612dfdcb2efffe3ce19aa3bebe
YuWeiKuo <adppss90045@gmail.com>
2023-11-12 16:34:53 +0800

    修改hexoc.sh

--- hexoc.sh
+++ hexoc.sh
@@ -3,9 +3,10 @@
 if [ $1 = 's' ]
 then
 	echo localhost
-	hexo clean && hexo g && hexo s
+	echo after generate hexo data, deploy localhost and open it at browser
+	hexo clean && ./source/_posts/writeupdatetime.js && ./source/_posts/updateFileTime.js && hexo g && hexo s -o
 elif [ $1 = 'd' ]
 then 
 	echo deploy
-	hexo clean && hexo g && hexo d
+	hexo clean && ./source/_posts/writeupdatetime.js && ./source/_posts/updateFileTime.js && hexo g && hexo d
 fi
