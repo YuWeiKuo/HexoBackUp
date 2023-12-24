5a0421000fca1f11d13ee1de99d08df3f2362e78
YuWeiKuo <adppss90045@gmail.com>
2023-12-24 19:06:03 +0800

    更新hexoc.sh

--- hexoc.sh
+++ hexoc.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
-if [ $1 = 's' ]
+if [ $1 = 's update' ]
+then
+	echo localhost and update time
+	echo after generate hexo data, deploy localhost and open it at browser
+	#hexo clean && ./source/_posts/updateFileTime.js
+	#hexo clean && ./source/_posts/writeupdatetime.js
+	hexo clean && ./source/_posts/writeupdatetime.js && ./source/_posts/updateFileTime.js && ./source/about/writeupdatetime.js && ./source/about/updateFileTime.js && hexo g && hexo s -o
+elif [ $1 = 'd update' ]
+then 
+	echo deploy and update time
+	hexo clean && ./source/_posts/writeupdatetime.js && ./source/_posts/updateFileTime.js  && ./source/about/writeupdatetime.js && ./source/about/updateFileTime.js && hexo g && hexo d
+elif [ $1 = 's' ]
 then
 	echo localhost
 	echo after generate hexo data, deploy localhost and open it at browser
-	hexo clean && ./source/_posts/writeupdatetime.js && ./source/_posts/updateFileTime.js && hexo g && hexo s -o
+	hexo g && hexo s -o
 elif [ $1 = 'd' ]
-then 
+then
 	echo deploy
-	hexo clean && ./source/_posts/writeupdatetime.js && ./source/_posts/updateFileTime.js && hexo g && hexo d
+	echo after generate hexo data, deploy localhost and open it at browser
+	hexo g && hexo d
+elif [ $1 = 's about' ]
+then
+	echo localhost and update time
+	echo after generate hexo data, deploy localhost and open it at browser
+	#hexo clean && ./source/_posts/updateFileTime.js
+	#hexo clean && ./source/_posts/writeupdatetime.js
+	hexo clean && ./source/about/writeupdatetime.js && ./source/about/updateFileTime.js && hexo g && hexo s -o
+elif [ $1 = 'd about' ]
+then 
+	echo deploy and update time
+	hexo clean && ./source/about/writeupdatetime.js && ./source/about/updateFileTime.js && hexo g && hexo d
+
 fi
